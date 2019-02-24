41c2a495147ff64ce00ceeb86bedad3d52646db7
silvestrevivo <silvestrevivomillan@gmail.com>
2019-02-24 20:38:34 +0100

    initial kick off for testing

---
+++ src/Congrats.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * Functional react component for congratulatory message
+ * @function
+ * @returns {JSX.Element}
+ */
+
+export default () => {
+  return (
+    <div>
+      Congrats
+    </div>
+  )
+}
