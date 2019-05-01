19355d15bb432c01e2fb16f870a3fb1a02b8b92d
silvestrevivo <silvestrevivomillan@gmail.com>
2019-05-01 10:14:48 +0200

    tests for guessedWord component

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import { shallow } from 'enzyme';
+import React from "react";
+import { shallow } from "enzyme";
 
-import { findByTestAttr, checkProps } from '../test/testUtils';
-import Congrats from './Congrats'
+import { findByTestAttr, checkProps } from "../test/testUtils";
+import Congrats from "./Congrats";
 
 const defaultProps = { success: false };
 
@@ -14,29 +14,28 @@ const defaultProps = { success: false };
  */
 const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<Congrats {...setupProps} />)
-}
+  return shallow(<Congrats {...setupProps} />);
+};
 
-test('renders without errors', () => {
+test("renders without errors", () => {
   const wrapper = setup();
-  const component = findByTestAttr(wrapper, 'component-congrats');
+  const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
-})
+});
 
-test('renders no text when `success` props is false', () => {
+test("renders no text when `success` props is false", () => {
   const wrapper = setup({ success: false });
-  const component = findByTestAttr(wrapper, 'component-congrats');
-  expect(component.text()).toBe('');
-})
+  const component = findByTestAttr(wrapper, "component-congrats");
+  expect(component.text()).toBe("");
+});
 
-test('renders non-empty congrats message when `success` prop is true', () => {
+test("renders non-empty congrats message when `success` prop is true", () => {
   const wrapper = setup({ success: true });
-  const message = findByTestAttr(wrapper, 'congrats-message');
+  const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
-})
+});
 
-test('does not throw warning with expected props', () => {
+test("does not throw warning with expected props", () => {
   const expectedProps = { success: false };
   checkProps(Congrats, expectedProps);
-})
-
+});
