85b30d7bda146c58bcad6c344bb2a0f097d40da9
silvestrevivo <silvestrevivomillan@gmail.com>
2019-05-01 15:39:56 +0200

    redux set up installation

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of guessed words
+ * @param {object} action - Action to be reduced
+ * @returns {boolean} - new success state
+ */
+
+export default (state, action) => {
+  return null;
+};
